0b584e59b051d1419bde0d853c01255246e6b9ea
Anwesh43 <anweshthecool0@gmail.com>
2026-09-23 15:24:40 +0530

    adding arxiv_tool

---
+++ arxiv_papers_ai_agent/tools/arxiv_tool.py
@@ -0,0 +1,16 @@
+from services.arxiv_service import ArxivService 
+
+arxivService = ArxivService()
+
+def searchForPapers(query : str):
+    print(f"Calling searchForPapers for {query}")
+    return arxivService.searchForPapers(query)
+
+def writeHTML(htmlStr : str, fileName : str):
+    print(f"Calling writeHTML tool for {fileName}")
+    with open(fileName, "w") as f:
+        f.write(htmlStr)
+    return {
+        "status": "success",
+        "message": f"Written html to {fileName}, don't display html to fileName"
+    }
